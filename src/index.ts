b993556c09004d5398bf7c56d9666c3010cf1d6b
pharez ayodele <pharezayodele6@gmail.com>
2025-05-18 13:17:04 +0100

    Fix: Use Heroku PORT environment variable for server

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config({
   });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Setup knex instance using the development config
 const environment = process.env.NODE_ENV || 'development';
